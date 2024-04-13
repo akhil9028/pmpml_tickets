499f9407323d5222ae72fb5f3d861f65600bd60f
akhil9028 <111130878+akhil9028@users.noreply.github.com>
2024-04-14 01:49:55 +0530

    Delete models directory

--- models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const newSchemas = new mongoose.Schema({
-  tickets: Number,
-  amount: Number,
-  order_id: String,
-  payment_id: String,
-});
-
-module.exports = mongoose.model("User", newSchemas);
