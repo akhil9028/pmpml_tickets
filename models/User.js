63cff3b0df816db2017f5cd6d489305d58c00d84
akhil9028 <111130878+akhil9028@users.noreply.github.com>
2024-04-14 22:11:37 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const newSchemas = new mongoose.Schema({
+  tickets: Number,
+  amount: Number,
+  order_id: String,
+  payment_id: String,
+});
+
+module.exports = mongoose.model("User", newSchemas);
