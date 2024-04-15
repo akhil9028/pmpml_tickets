a862b71285d29287125d85f7ff542435bc3aa8c4
akhil9028 <111130878+akhil9028@users.noreply.github.com>
2024-04-15 08:19:48 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,21 +1,25 @@
 const mongoose = require("mongoose");
-const MongoClient = require("mongodb").MongoClient; //changed now
+const MongoClient = require("mongodb").MongoClient;
 
 mongoose.set("strictQuery", false);
-const mongoURI = process.env.MONGO_URI; //changed now
-const client = new MongoClient(mongoURI); //changed now
+const mongoURI = process.env.MONGO_URI;
+const client = new MongoClient(mongoURI);
+
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDb Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     process.exit(1);
   }
 };
+
 function getDb(dbName = process.env.DB_NAME) {
   return client.db(dbName);
 }
 
-module.exports = connectDB;
-module.exports = getDb;
+module.exports = {
+  connectDB,
+  getDb
+};
